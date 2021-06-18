50752d51e4a45cd487aa91a511eaaf761556cdfb
Raghavendra-03 <84341044+Raghavendra-03@users.noreply.github.com>
2021-06-18 22:39:37 +0530

    Create Total and average.py

---
+++ Total and average.py
@@ -0,0 +1,13 @@
+a = float(input("Enter a value: "))
+
+b = float(input("Enter b value: "))
+
+c = float(input("Enter c value: "))
+
+sum = (a + b + c)
+
+avg = (a + b + c)/3
+
+print(f' Total value = ', sum)
+
+print(f' Average value = ', avg)
